05f9b0c06ae156efe99938a28b675101d64559ae
Rodrigo Emygdio <rodrigo.emygdio@gmail.com>
2019-02-15 06:21:45 -0200

    Iniciando Projeto

--- PocClearScriptRitaJs/Program.cs
+++ PocClearScriptRitaJs/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using System.IO;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.NodeServices;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace PocClearScriptRitaJs
 {
+    /// <summary>
+    /// https://www.c-sharpcorner.com/article/net-core-html-to-pdf-generation-using-node/
+    /// https://github.com/aspnet/JavaScriptServices/tree/master/src/Microsoft.AspNetCore.NodeServices
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddNodeServices();
+
+            var serivceProvider = serviceCollection.BuildServiceProvider();
+            var nodeService = serivceProvider.GetService<INodeServices>();
         }
     }
 }
